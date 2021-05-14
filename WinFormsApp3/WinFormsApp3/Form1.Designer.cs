6a4d64f27d067073b7b61882b6dde5e6b600bbe3
Maksymilian <83972588+61889@users.noreply.github.com>
2021-05-14 22:19:17 +0200

    Add files via upload

---
+++ WinFormsApp3/WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿
+namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.n1 = new System.Windows.Forms.Button();
+            this.n2 = new System.Windows.Forms.Button();
+            this.n3 = new System.Windows.Forms.Button();
+            this.n4 = new System.Windows.Forms.Button();
+            this.n5 = new System.Windows.Forms.Button();
+            this.n6 = new System.Windows.Forms.Button();
+            this.n7 = new System.Windows.Forms.Button();
+            this.n8 = new System.Windows.Forms.Button();
+            this.n9 = new System.Windows.Forms.Button();
+            this.n0 = new System.Windows.Forms.Button();
+            this.bad = new System.Windows.Forms.Button();
+            this.bsub = new System.Windows.Forms.Button();
+            this.bmult = new System.Windows.Forms.Button();
+            this.bdiv = new System.Windows.Forms.Button();
+            this.bequal = new System.Windows.Forms.Button();
+            this.ndot = new System.Windows.Forms.Button();
+            this.bc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(25, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(359, 36);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // n1
+            // 
+            this.n1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n1.Location = new System.Drawing.Point(25, 81);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(67, 62);
+            this.n1.TabIndex = 1;
+            this.n1.Text = "1";
+            this.n1.UseVisualStyleBackColor = true;
+            this.n1.Click += new System.EventHandler(this.n1_Click);
+            // 
+            // n2
+            // 
+            this.n2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n2.Location = new System.Drawing.Point(98, 81);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(67, 62);
+            this.n2.TabIndex = 2;
+            this.n2.Text = "2";
+            this.n2.UseVisualStyleBackColor = true;
+            this.n2.Click += new System.EventHandler(this.n2_Click);
+            // 
+            // n3
+            // 
+            this.n3.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n3.Location = new System.Drawing.Point(171, 81);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(67, 62);
+            this.n3.TabIndex = 3;
+            this.n3.Text = "3";
+            this.n3.UseVisualStyleBackColor = true;
+            this.n3.Click += new System.EventHandler(this.n3_Click);
+            // 
+            // n4
+            // 
+            this.n4.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n4.Location = new System.Drawing.Point(25, 149);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(67, 62);
+            this.n4.TabIndex = 4;
+            this.n4.Text = "4";
+            this.n4.UseVisualStyleBackColor = true;
+            this.n4.Click += new System.EventHandler(this.n4_Click);
+            // 
+            // n5
+            // 
+            this.n5.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n5.Location = new System.Drawing.Point(98, 149);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(67, 62);
+            this.n5.TabIndex = 5;
+            this.n5.Text = "5";
+            this.n5.UseVisualStyleBackColor = true;
+            this.n5.Click += new System.EventHandler(this.n5_Click);
+            // 
+            // n6
+            // 
+            this.n6.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n6.Location = new System.Drawing.Point(171, 149);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(67, 62);
+            this.n6.TabIndex = 6;
+            this.n6.Text = "6";
+            this.n6.UseVisualStyleBackColor = true;
+            this.n6.Click += new System.EventHandler(this.n6_Click);
+            // 
+            // n7
+            // 
+            this.n7.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n7.Location = new System.Drawing.Point(25, 217);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(67, 62);
+            this.n7.TabIndex = 7;
+            this.n7.Text = "7";
+            this.n7.UseVisualStyleBackColor = true;
+            this.n7.Click += new System.EventHandler(this.n7_Click);
+            // 
+            // n8
+            // 
+            this.n8.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n8.Location = new System.Drawing.Point(98, 217);
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(67, 62);
+            this.n8.TabIndex = 8;
+            this.n8.Text = "8";
+            this.n8.UseVisualStyleBackColor = true;
+            this.n8.Click += new System.EventHandler(this.n8_Click);
+            // 
+            // n9
+            // 
+            this.n9.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n9.Location = new System.Drawing.Point(171, 217);
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(67, 62);
+            this.n9.TabIndex = 9;
+            this.n9.Text = "9";
+            this.n9.UseVisualStyleBackColor = true;
+            this.n9.Click += new System.EventHandler(this.n9_Click);
+            // 
+            // n0
+            // 
+            this.n0.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.n0.Location = new System.Drawing.Point(98, 285);
+            this.n0.Name = "n0";
+            this.n0.Size = new System.Drawing.Size(140, 62);
+            this.n0.TabIndex = 10;
+            this.n0.Text = "0";
+            this.n0.UseVisualStyleBackColor = true;
+            this.n0.Click += new System.EventHandler(this.n0_Click);
+            // 
+            // bad
+            // 
+            this.bad.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bad.Location = new System.Drawing.Point(244, 81);
+            this.bad.Name = "bad";
+            this.bad.Size = new System.Drawing.Size(67, 130);
+            this.bad.TabIndex = 11;
+            this.bad.Text = "+";
+            this.bad.UseVisualStyleBackColor = true;
+            this.bad.Click += new System.EventHandler(this.bad_Click);
+            // 
+            // bsub
+            // 
+            this.bsub.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bsub.Location = new System.Drawing.Point(244, 217);
+            this.bsub.Name = "bsub";
+            this.bsub.Size = new System.Drawing.Size(67, 62);
+            this.bsub.TabIndex = 12;
+            this.bsub.Text = "-";
+            this.bsub.UseVisualStyleBackColor = true;
+            this.bsub.Click += new System.EventHandler(this.bsub_Click);
+            // 
+            // bmult
+            // 
+            this.bmult.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bmult.Location = new System.Drawing.Point(317, 149);
+            this.bmult.Name = "bmult";
+            this.bmult.Size = new System.Drawing.Size(67, 62);
+            this.bmult.TabIndex = 13;
+            this.bmult.Text = "*";
+            this.bmult.UseVisualStyleBackColor = true;
+            this.bmult.Click += new System.EventHandler(this.bmult_Click);
+            // 
+            // bdiv
+            // 
+            this.bdiv.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bdiv.Location = new System.Drawing.Point(317, 217);
+            this.bdiv.Name = "bdiv";
+            this.bdiv.Size = new System.Drawing.Size(67, 62);
+            this.bdiv.TabIndex = 14;
+            this.bdiv.Text = "/";
+            this.bdiv.UseVisualStyleBackColor = true;
+            this.bdiv.Click += new System.EventHandler(this.bdiv_Click);
+            // 
+            // bequal
+            // 
+            this.bequal.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bequal.Location = new System.Drawing.Point(244, 285);
+            this.bequal.Name = "bequal";
+            this.bequal.Size = new System.Drawing.Size(140, 62);
+            this.bequal.TabIndex = 15;
+            this.bequal.Text = "=";
+            this.bequal.UseVisualStyleBackColor = true;
+            this.bequal.Click += new System.EventHandler(this.bequal_Click);
+            // 
+            // ndot
+            // 
+            this.ndot.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ndot.Location = new System.Drawing.Point(25, 285);
+            this.ndot.Name = "ndot";
+            this.ndot.Size = new System.Drawing.Size(67, 62);
+            this.ndot.TabIndex = 16;
+            this.ndot.Text = ",";
+            this.ndot.UseVisualStyleBackColor = true;
+            this.ndot.Click += new System.EventHandler(this.ndot_Click);
+            // 
+            // bc
+            // 
+            this.bc.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bc.Location = new System.Drawing.Point(317, 81);
+            this.bc.Name = "bc";
+            this.bc.Size = new System.Drawing.Size(67, 62);
+            this.bc.TabIndex = 17;
+            this.bc.Text = "C";
+            this.bc.UseVisualStyleBackColor = true;
+            this.bc.Click += new System.EventHandler(this.bc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 376);
+            this.Controls.Add(this.bc);
+            this.Controls.Add(this.ndot);
+            this.Controls.Add(this.bequal);
+            this.Controls.Add(this.bdiv);
+            this.Controls.Add(this.bmult);
+            this.Controls.Add(this.bsub);
+            this.Controls.Add(this.bad);
+            this.Controls.Add(this.n0);
+            this.Controls.Add(this.n9);
+            this.Controls.Add(this.n8);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.n6);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Kalkulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button n1;
+        private System.Windows.Forms.Button n2;
+        private System.Windows.Forms.Button n3;
+        private System.Windows.Forms.Button n4;
+        private System.Windows.Forms.Button n5;
+        private System.Windows.Forms.Button n6;
+        private System.Windows.Forms.Button n7;
+        private System.Windows.Forms.Button n8;
+        private System.Windows.Forms.Button n9;
+        private System.Windows.Forms.Button n0;
+        private System.Windows.Forms.Button bad;
+        private System.Windows.Forms.Button bsub;
+        private System.Windows.Forms.Button bmult;
+        private System.Windows.Forms.Button bdiv;
+        private System.Windows.Forms.Button bequal;
+        private System.Windows.Forms.Button ndot;
+        private System.Windows.Forms.Button bc;
+    }
+}
+
